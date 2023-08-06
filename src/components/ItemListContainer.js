37224cf9c0aadf3154a825917511df331eebcab9
Rocio <rb.caffiero@gmail.com>
2023-08-06 18:07:54 -0300

    test

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const ItemListContainer = () => {
-    let greeting = "Bienvenidos al listado de Productos!"
+    let greeting = "Bienvenidos al listado de Productos"
   return (
     <div>{greeting}</div>
   )
